ea7043e72e4a071443632cab9eed3461523925f7
Drennout <serega10lad@gmail.com>
2020-10-22 08:58:42 +0300

    авл деревоЭ Э

---
+++ avlTree/cmake-build-debug/CMakeFiles/avlTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avlTree.dir/main.cpp.o"
+  "avlTree"
+  "avlTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avlTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
